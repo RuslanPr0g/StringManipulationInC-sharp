431f8188152a642dee7872416a76ad6c16b3a55d
Ruslan Rudenko <ruds@opinov8.com>
2021-09-20 17:41:26 +0300

    Performance count for string builder vs regular string concatenation

--- CsharpStringManipulations/Program.cs
+++ CsharpStringManipulations/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.Security.Claims;
+using System.Text;
 
 namespace CsharpStringManipulations
 {
@@ -10,6 +12,11 @@ namespace CsharpStringManipulations
         {
             var text = StringConversion("StrinG To tEsT yOu;");
             Console.WriteLine(text);
+
+            Console.WriteLine("# Performance counting:");
+            ConcatStringPerformance();
+            Console.WriteLine();
+            StringBuilderPerformance();
         }
 
         private static string StringConversion(string text)
@@ -21,6 +28,38 @@ namespace CsharpStringManipulations
             result = textInfo.ToTitleCase(text);
             
             return result;
+        }
+
+        private static void ConcatStringPerformance()
+        {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+
+            var test = string.Empty;
+
+            for (var i = 0; i < 100000; i++)
+            {
+                test += i;
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"- Regular concatenation string stopwatch: { stopwatch.ElapsedMilliseconds }ms");
+        }
+
+        private static void StringBuilderPerformance()
+        {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+
+            StringBuilder test = new();
+
+            for (var i = 0; i < 100000; i++)
+            {
+                test.Append(i);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"- Regular string builder stopwatch: { stopwatch.ElapsedMilliseconds }ms");
         }
     }
 }
